d09738cee8e0e493825e60b3fbf61b65da1f5d7a
Michael Osarumwense <sanmicad@gmail.com>
2024-03-25 15:20:15 +0000

    updated profile and added gunicorn

--- run.py
+++ run.py
@@ -4,13 +4,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
+# Set default values for IP and PORT
+default_ip = '0.0.0.0'
+default_port = 5000
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=False)
+    # Retrieve IP and PORT from environment variables or use defaults
+    host = os.environ.get('IP', default_ip)
+    port = int(os.environ.get('PORT', default_port))
+
+    app.run(host=host, port=port, debug=True)
